e7d2646d98280292096d5e8c4a308c23fe897521
Hambalkó Bence <hambalko.bence@gmail.com>
2020-02-18 00:34:19 +0100

    asd

--- Pizza/main.py
+++ Pizza/main.py
@@ -12,9 +12,9 @@ def main():
     pizzatypes = lines[1].split(' ')
     
     # Driver program to test above function 
-    val = [60, 100, 120] 
-    wt = [10, 20, 30] 
-    W = 50
+    val = [1, 2, 3, 4] 
+    wt = [1, 2, 3, 4]
+    W = 100
     n = len(val) 
     print(knapSack(W, wt, val, n)) 
 
@@ -27,25 +27,32 @@ def main():
 # be put in a knapsack of capacity W 
 def knapSack(W, wt, val, n):
     K = [[0 for x in range(W + 1)] for x in range(n + 1)] 
+
+    asd = [[list() for x in range(W + 1)] for x in range(n + 1)] 
   
     # Build table K[][] in bottom up manner 
     for i in range(n + 1): 
         for w in range(W + 1): 
             if i == 0 or w == 0: 
-                K[i][w] = (0, list())
+                K[i][w] = 0
             elif wt[i-1] <= w: 
-                if val[i-1] + K[i-1][w-wt[i-1]] > K[i-1][w]:
-                    K[i][w][1].append(val[i-1] + K[i-1][w-wt[i-1]])
-                    K[i][w] = (val[i-1] + K[i-1][w-wt[i-1]], K[i][w][1])
+                if val[i-1] + K[i-1][w-wt[i-1]] >= K[i-1][w]:
+                    asd[i][w].append(K[i-1][w-wt[i-1]])
+                    asd[i][w].append(val[i-1])
+                    K[i][w] = val[i-1] + K[i-1][w-wt[i-1]]
                 else:
-                    K[i][w][1].append(K[i-1][w])
-                    K[i][w] = (K[i-1][w], K[i][w][1])
+                    #K[i-1][w][1].append(K[i-1][w][0])
+                    for item in asd[i-1][w]:
+                        asd[i][w].append(item)
+                    K[i][w] = K[i-1][w]
                 #K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]) 
             else: 
-                K[i][w][1].append(K[i-1][w])
-                K[i][w] = (K[i-1][w], K[i][w][1]) 
+                #K[i-1][w][1].append(K[i-1][w][0])
+                for item in asd[i-1][w]:
+                    asd[i][w].append(item)
+                K[i][w] = K[i-1][w] 
   
-    return K[n][W] 
+    return K[n][W]
 
 if __name__ == "__main__":
     main()

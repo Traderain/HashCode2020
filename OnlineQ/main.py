fa4fceff564183ca71eb3ca93d192e7d2e388944
Hambalkó Bence <hambalko.bence@gmail.com>
2020-02-20 20:05:34 +0100

    Working fitness implementation

--- OnlineQ/main.py
+++ OnlineQ/main.py
@@ -1,19 +1,36 @@
 import io
 
 class Library:
+    id = 0
     bookcount = 0
     signuplen = 0
     ship = 0
     books = list()
 
     def __init__(self):
+        self.id = 0
         self.bookcount = 0
         self.signuplen = 0
         self.ship = 0
         self.books = list()
 
+    # Fitness of the library
     def fitness(self):
         return  self.ship * self.bookcount * ( 1 / self.signuplen)
+
+    # Returns the books it sends for x days
+    def getbook(self, fordays):
+        max = self.ship * fordays
+        if max > len(self.books):
+            return self.books
+        else:
+            ret = list()
+            for i in range(0, max):
+                ret.append(self.books[i])
+            return ret
+    
+    def toSchippingSchedule(self, fordays):
+        return (self.id, self.getbook(fordays))
 
 
 
@@ -76,6 +93,7 @@ def main():
         linenum = 1 + i*2 + 1
         
         l = Library()
+        l.id = i
         lsplit = lines[linenum].split(' ')
         l.bookcount = int(lsplit[0].strip())
         l.signuplen = int(lsplit[1].strip())
@@ -84,17 +102,19 @@ def main():
         for book in lines[linenum+1].split(' '):
             l.books.append(int(book.strip()))
         sch.libs.append(l)
-    print('Meme')
     ss = ShippingSchedule()
 
-    vals = list()
-    vals.append(0)
-    vals.append(1)
-    vals.append(2)
-    ss.sends.append((0,vals))
-    ss.sends.append((0,vals))
-    ss.sends.append((0,vals))
-
+    sch.libs.sort(key = (lambda a : a.fitness()))
+    done = False
+    iter = 0
+    currdays = 0
+    while not done:
+        elem = sch.libs[iter]
+        if elem.signuplen + currdays < sch.days:
+            ss.sends.append(elem.toSchippingSchedule(sch.days - (currdays + elem.signuplen)))
+            currdays = currdays + elem.signuplen
+        else:
+            done = True
     ss.printtofile('test.out')
 
 
